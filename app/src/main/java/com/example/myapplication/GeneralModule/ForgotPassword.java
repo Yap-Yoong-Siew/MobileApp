be5b691fd00f35bf7f351a74e64a3d220ae0d915
Yap-Yoong-Siew <s2122194@siswa.um.edu.my>
2023-01-03 09:18:00 +0800

    even more github commetn
    github comment for demo

--- app/src/main/java/com/example/myapplication/GeneralModule/ForgotPassword.java
+++ app/src/main/java/com/example/myapplication/GeneralModule/ForgotPassword.java
@@ -14,7 +14,7 @@ public class ForgotPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
     }
-
+//even more github comment
     public void RestoreAccount(View view) {
     }
 }
